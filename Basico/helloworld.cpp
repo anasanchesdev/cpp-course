91d10ed609422fb071eccc5955bc08bccf960c94
Dev. Sanches <dev.anaclarasanches@gmail.com>
2025-08-20 10:36:21 -0300

    Initial commit

---
+++ Basico/helloworld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>  // Biblioteca com funções de Input e Output
+
+int main(){
+    /*
+    std: Standard
+    C: Character
+    Out: Output
+    <<: Indica que a informação é saída
+    endl: Endline, indica o final de uma string
+    */
+    std::cout << "Oiii!" << std::endl;
+    std::cout << "Tchauu!!" << '\n';
+    return 0;
+}
